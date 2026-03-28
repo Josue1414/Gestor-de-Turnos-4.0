83e7c6aee935527322669fbd2fb62d8d0aea45d3
Josue1414 <josue.trabajo14@gmail.com>
2026-03-28 01:05:58 -0600

    correcion de rutas y vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SuperAdminPanel from './views/SuperAdmin/SuperAdminPanel';
 import AdminPanel from './views/Admin/AdminPanel';
 import ParticipantPanel from './views/User/ParticipantPanel';
-import LoginScreen from './views/Login/LoginScreen';
-import InviteScreen from './views/User/InviteScreen'; // <-- NUEVO ARCHIVO QUE CREAREMOS
+import LoginScreen from './views/Login/LoginScreen'; 
+import InviteScreen from './views/User/InviteScreen'; // <-- IMPORTAMOS LA PANTALLA DE INVITACIÓN
 
 function App() {
   return (
@@ -13,13 +13,12 @@ function App() {
         <Route path="/super-admin" element={<SuperAdminPanel />} />
         <Route path="/admin/:id" element={<AdminPanel />} />
 
-        {/* --- 1. LINK GENERAL DE INVITACIÓN (Registro) --- */}
-        {/* URL Ej: misitio.com/invite/evento-123/admin-XYZ */}
-        <Route path="/invite/:eventoId/:adminId" element={<InviteScreen />} />
-
-        {/* --- 2. LINK ÚNICO PERSONAL (Panel del Participante) --- */}
-        {/* URL Ej: misitio.com/p/evento-123/admin-XYZ/part-987 */}
+        {/* --- NUEVAS RUTAS PARA PARTICIPANTES E INVITADOS --- */}
+        {/* Ruta para el link personal del participante */}
         <Route path="/p/:eventoId/:adminId/:participanteId" element={<ParticipantPanel />} />
+        
+        {/* Ruta general para que se registren los nuevos */}
+        <Route path="/invite/:eventoId/:adminId" element={<InviteScreen />} />
       </Routes>
     </Router>
   );
